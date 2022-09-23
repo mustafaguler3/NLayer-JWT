23284e37dbaae8ce976b6637190b5ac25feea1d5
mustafaguler3 <mustafa.glr@hotmail.com>
2022-09-23 17:39:05 +0300

    add userservice on service layer

---
+++ ServiceLayer/Services/UserService.cs
@@ -0,0 +1,56 @@
+﻿using CoreLayer.Dtos;
+using CoreLayer.Entities;
+using CoreLayer.Services;
+using Microsoft.AspNetCore.Identity;
+using ServiceLayer.Mappings;
+using Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services
+{
+	public class UserService : IUserService
+	{
+		private readonly UserManager<User> _userManager;
+
+		public UserService(UserManager<User> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		public async Task<Response<UserDto>> CreateUserAsync(CreateUserDto userDto)
+		{
+			var user = new User()
+			{
+				Email = userDto.Email,
+				UserName = userDto.UserName
+			};
+
+			var result = await _userManager.CreateAsync(user, userDto.Password);
+
+			if (!result.Succeeded)
+			{
+				var error = result.Errors.Select(i => i.Description).ToList();
+
+				return Response<UserDto>.Fail(new ErrorDto(error, true), 400);
+			}
+
+			return Response<UserDto>.Success(ObjectMapper.Mapper.Map<UserDto>(user),200);
+		}
+
+		public async Task<Response<UserDto>> GetUserByNameAsync(string userName)
+		{
+			var user = await _userManager.FindByNameAsync(userName);
+
+			if (user == null)
+			{
+				return Response<UserDto>.Fail("username not found",404,true);
+			}
+
+			return Response<UserDto>.Success(ObjectMapper.Mapper.Map<UserDto>(user), 200);
+		}
+	}
+}
